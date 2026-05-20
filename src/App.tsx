abec5d45cb71f8df51708bbde23fe200c346f822
ashley-perez <ashleyperez428@gmail.com>
2026-05-19 22:10:31 -0700

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import Profile from './components/Profile'
 import LinkCards from './components/LinkCards'
+import { cards } from './components/CardData'
+import type { CardData } from './components/CardData'
 
 function App() {
   // stuff here
 
   return (
-   <main className="flex flex-col px-4 w-full max-w-md mx-auto items-center">
+   <main className="flex flex-col items-center">
+    <div className="p-3 justify-center">
     <Profile />
-    <LinkCards />
+		</div>
+
+		<div className="flex flex-col gap-3">
+		{cards.map((card, index) => (
+				// rememeber that LinkCards {cardinfo}
+				<LinkCards key={index} cardinfo={card} />
+    ))}
+		</div>
+
    </main>
   );
 }
